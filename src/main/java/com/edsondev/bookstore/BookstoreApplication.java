02349492fba2526548a7284d207109adfb7e0926
Edson Sales <edsonsales99@gmail.com>
2022-01-06 18:08:35 -0500

    Criando conexão com o banco h2 e criando objetos na classe main

--- src/main/java/com/edsondev/bookstore/BookstoreApplication.java
+++ src/main/java/com/edsondev/bookstore/BookstoreApplication.java
@@ -1,13 +1,39 @@
 package com.edsondev.bookstore;
 
+import com.edsondev.bookstore.domain.Categoria;
+import com.edsondev.bookstore.domain.Livro;
+import com.edsondev.bookstore.repositories.CategoriaReposytory;
+import com.edsondev.bookstore.repositories.LivroReposytory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaReposytory categoriaReposytory;
+
+	@Autowired
+	LivroReposytory livroReposytory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+
+		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem inpsum", cat1);
+
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		this.categoriaReposytory.saveAll(Arrays.asList(cat1));
+		this.livroReposytory.saveAll(Arrays.asList(l1));
+
+	}
 }
