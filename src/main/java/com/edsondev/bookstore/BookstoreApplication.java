538ef16f8754de6cdd0be7a5881e4228e672b038
Edson Sales <edsonsales99@gmail.com>
2022-01-06 10:01:15 -0500

    Criando a estrutura inicial do projeto

---
+++ src/main/java/com/edsondev/bookstore/BookstoreApplication.java
@@ -0,0 +1,15 @@
+package com.edsondev.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe inicial do projeto*/
+@SpringBootApplication
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
